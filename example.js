9f314f11814a1e87478f9494734653ab62e08cb0
Alex Iskander <alex@Alex-Iskanders-Mac-Pro.local>
2009-12-19 14:37:14 -0600

    Very simple and but not really useful SproutCore-based long-polling server. Oddly, works without a run loop (no bindings).

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 // an attempt at a more practical test
-var sys = require("sys");
+var sys = require("sys"), http = require("http");
 
 var SC = require("./runtime").SC;
 process.mixin(GLOBAL, {
@@ -7,13 +7,45 @@ process.mixin(GLOBAL, {
   NO: false
 });
 
-var Roots = {
-  Root: SC.Object.extend({
-    name: "Alex",
-    init: function(){
-      sys.puts("init root: " + this.name);
+// this is pretty pathetic, but still, it is an example
+// no message queuing means it would never work, though :)
+var Server = SC.Object.extend({
+  value: "Hi",
+  waiters: [],
+  request: function(request, response){
+    if (request.uri.path == "/change") {
+      this.set("value", request.uri.params["to"]);
+      response.sendHeader(200, {"content-type": "text/plain"});
+      response.sendBody("Done!");
+      response.finish();
+    } else {
+      this.waiters.push(response);
+      var self=this;
+      setTimeout(function(){ 
+        self.finish(response);
+      }, 30000); // do it automatically after 30 seconds.
     }
-  })
-};
+  },
+  
+  valueDidChange: function() {
+    for (var i = 0; i < this.waiters.length; i++) {
+      this.finish(this.waiters[i]);
+    }
+  }.observes("value"),
+  
+  finish: function(waiter) {
+    if (waiter.didFinish) return;
+    waiter.sendHeader(200, {"content-type":"text/plain"});
+    waiter.sendBody("Value: " + this.get("value"));
+    waiter.finish();
+    waiter.didFinish = true;
+  }
+});
 
-var r = Roots.Root.create({name: "Hayden"});
+var server = Server.create({
+  value: "Hello, World!"
+});
+
+http.createServer(function (request, response) {
+  server.request(request, response);
+}).listen(8002);
